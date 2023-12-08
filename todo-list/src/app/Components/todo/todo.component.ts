9634a6c95f0f7236c7e62713efcb709909e06ab9
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2023-12-08 21:06:57 +0530

    UI for todo app is completed

--- todo-list/src/app/Components/todo/todo.component.ts
+++ todo-list/src/app/Components/todo/todo.component.ts
@@ -1,20 +1,24 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { Todo } from '../../Todo';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
   @Input() todo: Todo = new Todo('', '');
-
-
   @Output() deleteTodoEvent = new EventEmitter<Todo>();
+  @Output() updateTodoEvent = new EventEmitter<Todo>();
 
   handleDeleteBtnClick(todo:Todo) {
     this.deleteTodoEvent.emit(todo);
+  }
+
+  handleChange(todo:Todo) {
+    this.updateTodoEvent.emit(todo);
   }
 }
--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './Components/todo/todo.component';
 import {ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Todo } from './Todo';
+// import { BrowserStorageService } from './storage.service';
 
 @Component({
   selector: 'app-root',
@@ -13,104 +14,16 @@ import { Todo } from './Todo';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-list';
-  todos: Todo[] = [{
-    id: 1,
-    title: 'Todo 1',
-    desc: 'Todo 1 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 2,
-    title: 'Todo 2',
-    desc: 'Todo 2 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 3,
-    title: 'Todo 3',
-    desc: 'Todo 3 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 4,
-    title: 'Todo 4',
-    desc: 'Todo 4 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 5,
-    title: 'Todo 5',
-    desc: 'Todo 5 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 6,
-    title: 'Todo 6',
-    desc: 'Todo 6 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 7,
-    title: 'Todo 7',
-    desc: 'Todo 7 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 8,
-    title: 'Todo 8',
-    desc: 'Todo 8 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 9,
-    title: 'Todo 9',
-    desc: 'Todo 9 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 10,
-    title: 'Todo 10',
-    desc: 'Todo 10 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 11,
-    title: 'Todo 11',
-    desc: 'Todo 11 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 12,
-    title: 'Todo 12',
-    desc: 'Todo 12 description',
-    completed: false,
-    date: new Date()
-  },
-  {
-    id: 13,
-    title: 'Todo 13',
-    desc: 'Todo 13 description',
-    completed: false,
-    date: new Date()
-  },];
 
-  
+  // constructor(private localStore : BrowserStorageService) {}
+
+  title = 'todo-list';
+  // todos : Todo[] = JSON.parse(this.localStore.get("todos") as string)  || [];
+  todos : Todo[] = [];
 
   todoForm = new FormGroup({
     title : new FormControl('', Validators.required),
-    desc : new FormControl('', Validators.required),    
+    desc : new FormControl('', Validators.required), 
   })
 
   handleSubmit() {
@@ -118,12 +31,23 @@ export class AppComponent {
       let todo = new Todo(this.todoForm.value.title as string, this.todoForm.value.desc as string);
       todo.id = this.todos.length + 1;
       this.todos.push(todo);
+
+      this.todoForm.reset();
+
+      // this.localStore.set("todos", JSON.stringify(this.todos));
     }
   }
 
   deleteItem(todo: Todo) {
     const index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);
+    // this.localStore.set("todos", JSON.stringify(this.todos));
+  }
+
+  updateItem(todo :Todo) {
+    const index = this.todos.indexOf(todo);
+    this.todos[index].completed = !todo.completed;
+    // this.localStore.set("todos", JSON.stringify(this.todos));
   }
 
 }
